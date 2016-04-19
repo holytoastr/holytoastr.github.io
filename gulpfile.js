699534ab6ee188b794f68284a1d37cb0cc904f0a
Melissa Miller <lis.d.miller@gmail.com>
2016-04-19 13:55:39 -0400

    sets up gulp file for sass

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function() {
+  return gulp.src('./css/**/*.scss')
+  .pipe(sass().on('error', sass.logError))
+  .pipe(gulp.dest('./css/'));
+});
+
+gulp.task('default', function() {
+
+});
