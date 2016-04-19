70fbbedbe59e2e0ddc48c7b14427edfcd81989cc
Melissa Miller <lis.d.miller@gmail.com>
2016-04-19 14:00:24 -0400

    Adds watch and connect to gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,23 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var watch = require('gulp-watch');
+var connect = require('gulp-connect');
 
-gulp.task('sass', function() {
-  return gulp.src('./css/**/*.scss')
-  .pipe(sass().on('error', sass.logError))
-  .pipe(gulp.dest('./css/'));
+gulp.task('sass', function () {
+ return gulp.src('./css/**/*.scss')
+   .pipe(sass().on('error', sass.logError))
+   .pipe(gulp.dest('./css/'))
+   .pipe(connect.reload());
 });
 
-gulp.task('default', function() {
-
+gulp.task('watch', function () {
+ gulp.watch('./css/**/*.scss', ['sass']);
 });
+
+gulp.task('connect', function() {
+  connect.server({
+    livereload: true
+  });
+});
+
+gulp.task('default', ['sass', 'connect', 'watch']);
